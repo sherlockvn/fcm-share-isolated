b1f8d36da7cee0a0898573bacd6783d5d380e855
Lukas Lihotzki <lukas@lihotzki.de>
2021-04-13 15:57:59 +0200

    feat: null safety

--- lib/fcm_shared_isolate.dart
+++ lib/fcm_shared_isolate.dart
@@ -5,18 +5,19 @@ import 'package:flutter/services.dart';
 class FcmSharedIsolate {
   final _channel = MethodChannel('fcm_shared_isolate');
   final _msg = <Map<dynamic, dynamic>>[];
-  void Function(Map<dynamic, dynamic>) _onMessage;
-  void Function(String) _onNewToken;
+  void Function(Map<dynamic, dynamic>)? _onMessage;
+  void Function(String)? _onNewToken;
 
   FcmSharedIsolate() {
     _channel.setMethodCallHandler(handle);
   }
 
-  Future<dynamic> handle(MethodCall call) {
+  Future<dynamic> handle(MethodCall call) async {
     if (call.method == 'message') {
       final Map<dynamic, dynamic> data = call.arguments;
-      if (_onMessage != null) {
-        _onMessage(data);
+      final onMessage = _onMessage;
+      if (onMessage != null) {
+        onMessage(data);
       } else {
         _msg.add(data);
       }
@@ -32,13 +33,13 @@ class FcmSharedIsolate {
   }
 
   void setListeners({
-    void Function(Map<dynamic, dynamic>) onMessage,
-    void Function(String) onNewToken,
+    void Function(Map<dynamic, dynamic>)? onMessage,
+    void Function(String)? onNewToken,
   }) {
     _onMessage = onMessage;
     _onNewToken = onNewToken;
-    if (_onMessage != null) {
-      _msg.forEach(_onMessage);
+    if (onMessage != null) {
+      _msg.forEach(onMessage);
       _msg.clear();
     }
   }
